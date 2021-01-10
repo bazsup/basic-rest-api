0f2ce0792410eafac70d5514d82143ebc438873b
bazsup <supawit.ruen@mail.kmutt.ac.th>
2021-01-11 00:04:55 +0700

    set default value for user.created_at

--- crud-with-go/db/migrations/000001_create_users_table.up.sql
+++ crud-with-go/db/migrations/000001_create_users_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS users (
     id int NOT NULL AUTO_INCREMENT,
     name varchar(255) NOT NULL,
-    created_at timestamp NOT NULL,
+    created_at timestamp DEFAULT '0000-00-00 00:00:00',
     updated_at timestamp NOT NULL,
     PRIMARY KEY (id)
 );
